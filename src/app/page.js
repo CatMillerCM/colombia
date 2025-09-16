cbc4626ef94cba6bb5bcc5808002e67bbb1dfa72
Cat Miller <catmiller.cm@gmail.com>
2025-09-16 20:44:25 +0200

    add basic dropdown with 1 - 15 options

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,35 @@
+'use client';
+
+import { useState } from 'react';
 import styles from './entry.module.css';
 
 const Page = () => {
+  const [playerCount, setPlayerCount] = useState(null);
+
   return (
     <main className={styles.main}>
+      <div>
+        <select
+          className={styles.input}
+          value={playerCount}
+          onChange={e => setPlayerCount(e.target.value)}
+        >
+          <option value="" disabled>Select number of players</option>
+          {[...Array(15)].map((_, i) => (
+            <option key={i + 1} value={i + 1}>{i + 1}</option>
+          ))}
+        </select>
+        <button
+          className={styles.button}
+          type="button"
+          onClick={{}}
+          disabled={!playerCount}
+        >
+          Distribute ball numbers
+        </button>
+      </div>
     </main>
   );
-}
+};
 
 export default Page;
