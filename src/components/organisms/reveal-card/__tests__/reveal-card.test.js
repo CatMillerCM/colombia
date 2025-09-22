e2f755550ef740a63ff1fde99d029cff401c4070
Cat Miller <catmiller.cm@gmail.com>
2025-09-22 15:39:55 +0200

    add tests for reveal card

---
+++ src/components/organisms/reveal-card/__tests__/reveal-card.test.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { render, waitFor } from '@testing-library/react';
+import Numbers from '@/components/atoms/numbers';
+import RestartButton from '@/components/molecules/restart-button';
+import NextPlayerButton from '@/components/molecules/next-player-button';
+import RevealButton from '@/components/molecules/reveal-button';
+import RevealCard from '..';
+
+jest.mock('@/components/atoms/numbers');
+jest.mock('@/components/molecules/restart-button');
+jest.mock('@/components/molecules/next-player-button');
+jest.mock('@/components/molecules/reveal-button');
+
+describe('RevealCard', () => {
+  const setStepMock = jest.fn();
+  const setNumbersMock = jest.fn();
+  const distributions = { playerPots: [[1, 2], [3, 4]], discarded: [5] };
+  const step = 2;
+  let playerCount = 4;
+
+  const renderRevealCard = (step, playerCount) => {
+    render(
+      <RevealCard
+        distributions={distributions}
+        setStep={setStepMock}
+        step={step}
+        playerCount={playerCount}
+      />
+    );
+  };
+
+  beforeEach(() => {
+    Numbers.mockReturnValue(<div />);
+    RestartButton.mockReturnValue(<div />);
+    NextPlayerButton.mockReturnValue(<div />);
+    RevealButton.mockImplementation(jest.fn(({ setNumbers }) => {
+      setNumbersMock.mockImplementation(setNumbers);
+    return <div />;
+    }));
+
+    renderRevealCard(step, playerCount);
+  });
+
+  describe('when the numbers array is empty', () => {
+    it('should NOT render the Numbers, RestartButton or NextPlayerButton components', () => {
+      expect(Numbers).not.toHaveBeenCalled();
+      expect(RestartButton).not.toHaveBeenCalled();
+      expect(NextPlayerButton).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('when the numbers array contains numbers', () => {
+    beforeEach(async () => {
+      await waitFor(() => setNumbersMock([1, 2]));
+    });
+
+    it('should render the Numbers component', async () => {
+      expect(Numbers).toHaveBeenCalled();
+    });
+
+    describe('and the step count is less than the player count', () => {
+      it('should render the NextPlayerButton component', () => {
+        expect(NextPlayerButton).toHaveBeenCalled();
+      });
+    });
+
+    describe('and the step number is equal to the player count', () => {
+      beforeEach(async() => {
+        playerCount = 2;
+        renderRevealCard(step, playerCount);
+        
+        await waitFor(() => setNumbersMock([1, 2]));
+      });
+
+      it('should render the RestartButton component', () => {
+        expect(RestartButton).toHaveBeenCalled();
+      });
+    });
+  });
+});
