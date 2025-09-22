5e37ae30740e462243cf5e4e85e9588c99077ae6
Cat Miller <catmiller.cm@gmail.com>
2025-09-22 15:17:41 +0200

    add entry card tests

---
+++ src/components/organisms/entry-card/__tests__/entry-card.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render, waitFor } from '@testing-library/react';
+import RemainderWarning from '@/components/molecules/remainder-warning';
+import PlayersDropdown from '@/components/molecules/players-dropdown';
+import EntryCard from '..';
+
+jest.mock('@/components/molecules/remainder-warning');
+jest.mock('@/components/molecules/players-dropdown');
+
+describe('EntryCard', () => {
+  const setPlayerCountMock = jest.fn();
+  const setDistributionsMock = jest.fn();
+  const setStepMock = jest.fn();
+  const setHasRemainderMock = jest.fn();
+
+  beforeEach(() => {
+    RemainderWarning.mockReturnValue(<div />);
+    PlayersDropdown.mockImplementation(jest.fn(({ setHasRemainder }) => {
+      setHasRemainderMock.mockImplementation(setHasRemainder);
+    return <div />;
+    }));
+
+    render(
+      <EntryCard
+        playerCount={3}
+        setPlayerCount={setPlayerCountMock}
+        setDistributions={setDistributionsMock}
+        setStep={setStepMock}
+      />
+    );
+  });
+
+  describe('when there is no remainder', () => {
+    it('should NOT render the remainder warning component', () => {
+      expect(RemainderWarning).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('when there is a remainder', () => {
+    it('should render the remainder warning component', async () => {
+      await waitFor(() => setHasRemainderMock(true));
+
+      expect(RemainderWarning).toHaveBeenCalled();
+    });
+  });
+});
