a23896db628f624a9c27e1cb0b8685a5797b8edc
Cat Miller <catmiller.cm@gmail.com>
2025-09-29 23:08:03 +0200

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:jest/recommended'
+  ],
+  plugins: ['jest'],
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'comma-dangle': ['error', 'never'],
+    'eol-last': ['error', 'always'],
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }]
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src']
+        ],
+        extensions: ['.js', '.jsx']
+      }
+    }
+  }
+};
